a4ef03f9e17cf158fd837d3eb39a4dfe736ab3ac
Neha Yadav <ny663922@gmail.com>
2026-03-14 16:26:11 +0530

    learing to send html file

---
+++ view/view.js
@@ -0,0 +1,19 @@
+import express from "express";
+const app = express();
+const PORT = 5400;
+import path from "path";
+app.get("/", (req, res) => {
+  const absPath = path.resolve("view/file/home.html");
+  res.sendFile(absPath);
+});
+app.get("/login", (req, res) => {
+  const absPath = path.resolve("view/file/login.html");
+  res.sendFile(absPath);
+});
+app.get("/about", (req, res) => {
+  const absPath = path.resolve("view/file/about.html");
+  res.sendFile(absPath);
+});
+app.listen(PORT, () => {
+  console.log(`server is running on ${PORT}`);
+});

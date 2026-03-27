379b1f71358393d931ad3d1c45c4fe190627693d
Neha Yadav <ny663922@gmail.com>
2026-03-28 02:13:40 +0530

    cookies

--- pages/authication/auth.js
+++ pages/authication/auth.js
@@ -1,20 +1,30 @@
 import cookieParser from "cookie-parser";
 import express from "express";
 import bcrypt from "bcrypt";
-import jwt from "jwt";
+import jwt from "jsonwebtoken";
 const app = express();
 const PORT = 5400;
 
-// app.use(cookieParser());
+app.use(cookieParser());
 app.get("/", (req, res) => {
   //   res.cookie("name", "neha");
-  bcrypt.genSalt(10, function (err, salt) {
-    bcrypt.hash("pololololo", salt, function (err, hash) {
-      console.log(hash);
+  //   bcrypt.genSalt(10, function (err, salt) {
+  //     bcrypt.hash("pololololo", salt, function (err, hash) {
+  //       console.log(hash);
 
-      // Store hash in your password DB.
-    });
-  });
+  //       // Store hash in your password DB.
+  //     });
+  //   });
+  let token = jwt.sign({ email: "ny663922@gmail.com" }, "secret");
+  res.cookie("token", token);
+  console.log(token);
+  res.end("token");
+});
+app.get("/read", (req, res) => {
+  let data = jwt.verify(req.cookies.token, "secret");
+  console.log(data);
+
+  res.end("read");
 });
 
 app.listen(PORT, () => {
